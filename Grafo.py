183b934e92282f2ad7b49920464c8e17cd7717a7
gabrields1998 <gabrieldavidsacca@hotmail.com>
2019-04-04 22:51:29 -0300

    criacao ordem/remove

--- Grafo.py
+++ Grafo.py
@@ -1,9 +1,8 @@
-
+# --------------------------------------------------------------------------------------
 class Vertex:
-    self.__name = ""
-    self.__edges = []
 
     def __init__(self, name):
+        self.__edges = []
         self.__name = name
     
     def getName(self):
@@ -11,20 +10,48 @@ class Vertex:
 
     def getEdge(self):
         return self.__edges
+    
+    def removeEdge(self, name):
+        index = 0
+        for edge in self.__edges:
+            if name == edge.getName():
+                del(self.__edges[index])
+            index+=1
 
+    def printVertexName(self):
+        print("|", self.__name, "|", end = "")
+
+    def printAllEdge(self):
+        for edge in self.__edges:
+            print(" --> ", end = "")
+            edge.printVertexName()
+
+        print(" --> NULL")
+
+    def existEdge(self, name):
+        count = 0
+        for edge in self.__edges:
+            if edge.getName() == name:
+                count+=1
+        return count
+
+    def getOrdemEdge(self):
+        return len(self.__edges)
+
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------            
 class Grafo:
-    self.__list = []
-    self.__digrafo = True
-
+    
     def __init__(self):
-        pass
+        self.__vertex = []
+        self.__digrafo = True
 
-    def addVert(self, name):
-        self.__list.append(Vertex(name))
+    def addVertex(self, name):
+        self.__vertex.append(Vertex(name))
     
 
     def getVertex(self, name):
-        for vertex in self.__list:
+        for vertex in self.__vertex:
             if vertex.getName() == name:
                 return vertex
         return None
@@ -34,16 +61,70 @@ class Grafo:
         vInit = self.getVertex(vertInit)
 
         if vInit == None:
-            print("ALERT: Vertex" + vertInit + "undefined")
             return None
 
         vEnd = self.getVertex(vertEnd)
 
         if vEnd == None:
-            print("ALERT: Vertex" + vertEnd + "undefined")
             return None
 
         vInit.getEdge().append(vEnd)
-        print("Sucess")
         return 1
 
+    def printAll(self):
+        for vertex in self.__vertex:
+            vertex.printVertexName()
+            vertex.printAllEdge()
+
+    def __removeVertex(self, name):
+        index = 0
+        for vertex in self.__vertex:
+            if name == vertex.getName():
+                del(self.__vertex[index])
+            index+=1
+
+    def removeVertex(self, name):
+        for vertex in self.__vertex:
+            vertex.removeEdge(name)
+        self.__removeVertex(name)
+
+    def removeEdge(self, vertIni, vertEnd):
+        for vertex in self.__vertex:
+            if vertIni == vertex.getName():
+                vertex.removeEdge(vertEnd)
+    
+    def getOrdem(self, vert):
+        ordem = 0
+        for vertex in self.__vertex:
+            if vert == vertex.getName():
+               ordem += vertex.getOrdemEdge()
+            else: 
+                ordem += vertex.existEdge(vert)
+        return ordem
+# --------------------------------------------------------------------------------------
+
+gg = Grafo()
+
+gg.addVertex("1")
+gg.addVertex("2")
+gg.addVertex("3")
+gg.addVertex("4")
+gg.addVertex("5")
+
+gg.addEdge("1", "2")
+gg.addEdge("1", "3")
+gg.addEdge("3", "2")
+gg.addEdge("1", "2")
+gg.addEdge("2", "4")
+gg.addEdge("4", "2")
+gg.addEdge("3", "5")
+
+print("------------------------------------------")
+gg.printAll()
+
+# gg.removeVertex("2")
+print(gg.getOrdem("1"))
+print("------------------------------------------")
+gg.printAll()
+
+print("------------------------------------------")
